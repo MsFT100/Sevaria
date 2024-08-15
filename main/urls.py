765555a52f9888bce5d51e6242542c5fd0cd6797
MsFT100 <lennoxmathew@gmail.com>
2024-08-15 21:10:57 +0300

    checkout setup

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.Index.as_view(), name='home'),
-    
+    path('shop/', views.Shop.as_view(), name='shop'),
+    path('checkout/', views.CheckOut.as_view(), name='checkout'),
 ]
--- main/views.py
+++ main/views.py
@@ -6,3 +6,12 @@ class Index( View):
     def get(self, request):
         
         return render(request, 'pages/index.html')
+    
+
+class CheckOut(View):
+    def get(self, request):
+        return render(request, 'pages/checkout.html')
+    
+class Shop(View):
+    def get(self, request):
+        return render(request, 'pages/shop.html')
